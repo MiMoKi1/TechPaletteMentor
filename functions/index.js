acbeb05b2dc9b385b36a1a0022c3ef933961cb83
MiMoKi <134791551+MiMoKi1@users.noreply.github.com>
2024-10-18 22:44:24 +0100

    Add files via upload

--- functions/index.js
+++ functions/index.js
@@ -1,1 +1,22 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.checkPaidUser = functions.https.onCall(async (data, context) => {
+    const userEmail = data.email;
+
+    try {
+        const userDoc = await db.collection('paidUsers').doc(userEmail).get();
+        if (userDoc.exists) {
+            return { accessGranted: true };
+        } else {
+            return { accessGranted: false };
+        }
+    } catch (error) {
+        console.error('Error checking user:', error);
+        throw new functions.https.HttpsError('internal', 'Error checking user');
+    }
+});
