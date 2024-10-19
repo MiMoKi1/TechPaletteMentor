2e3d071c5ffcf56cda531b5f0261d69c889a1908
MiMoKi <134791551+MiMoKi1@users.noreply.github.com>
2024-10-19 10:51:45 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,35 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const cors = require('cors')({ origin: 'https://mimoki1.github.io' }); // Specify your allowed origin
+const cors = require('cors'); // Initialize cors without options here
 
 admin.initializeApp();
 
 exports.checkPaidUser = functions.https.onRequest((req, res) => {
-  // Use cors middleware
-  cors(req, res, () => {
-    if (req.method !== 'POST') {
-      return res.status(403).send('Forbidden!');
-    }
+    // Use CORS middleware with the specified origin
+    cors({ origin: 'https://mimoki1.github.io' })(req, res, () => {
+        console.log('Request body:', req.body); // Log the request body
 
-    const email = req.body.email;
-
-    // Check if the email exists in the Firestore collection
-    admin.firestore().collection('paidUser').doc(email).get()
-      .then(doc => {
-        if (doc.exists) {
-          // Access granted
-          return res.status(200).send({ accessGranted: true });
-        } else {
-          // Access denied
-          return res.status(200).send({ accessGranted: false });
+        // Check if the request method is POST
+        if (req.method !== 'POST') {
+            return res.status(403).send('Forbidden!');
         }
-      })
-      .catch(error => {
-        console.error("Error checking user access:", error);
-        return res.status(500).send("Internal Server Error");
-      });
-  });
+
+        const email = req.body.email;
+
+        // Check if the email exists in the Firestore collection
+        admin.firestore().collection('paidUser').doc(email).get()
+            .then(doc => {
+                if (doc.exists) {
+                    // Access granted
+                    return res.status(200).send({ accessGranted: true });
+                } else {
+                    // Access denied
+                    return res.status(200).send({ accessGranted: false });
+                }
+            })
+            .catch(error => {
+                console.error("Error checking user access:", error);
+                return res.status(500).send("Internal Server Error");
+            });
+    });
 });
